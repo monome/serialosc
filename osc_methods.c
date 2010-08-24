a7681b2d1a442e0ef6bd6578bd5aafdeb71c1845
William Light <will@illest.net>
2010-08-24 02:57:59 -0700

    added missing osc methods.  serialosc now passes libmonome OSC test.

--- osc_methods.c
+++ osc_methods.c
@@ -32,7 +32,6 @@ static int osc_clear_handler(const char *path, const char *types,
 	monome_t *monome = user_data;
 	int mode = (argc) ? argv[0]->i : 0;
 
-	puts("beep");
 	return monome_clear(monome, mode);
 }
 
@@ -56,16 +55,68 @@ static int osc_led_handler(const char *path, const char *types,
 		return monome_led_off(monome, argv[0]->i, argv[1]->i);
 }
 
+static int osc_led_col_handler(const char *path, const char *types,
+								   lo_arg **argv, int argc,
+								   lo_message data, void *user_data) {
+	monome_t *monome = user_data;
+	uint8_t buf[2] = {argv[1]->i};
 
-static int bail_if_false(int well_is_it) {
-	if( well_is_it )
-		return 0;
+	if( argc == 3 )
+		buf[1] = argv[2]->i;
+
+	return monome_led_col(monome, argv[0]->i, argc - 1, buf);
+}
+
+static int osc_led_row_handler(const char *path, const char *types,
+								   lo_arg **argv, int argc,
+								   lo_message data, void *user_data) {
+	monome_t *monome = user_data;
+	uint8_t buf[2] = {argv[1]->i};
+
+	if( argc == 3 )
+		buf[1] = argv[2]->i;
+
+	return monome_led_row(monome, argv[0]->i, argc - 1, buf);
+}
+
+static int osc_frame_handler(const char *path, const char *types,
+							 lo_arg **argv, int argc,
+							 lo_message data, void *user_data) {
+	monome_t *monome = user_data;
+	uint8_t buf[8];
+	uint i;
+
+	for( i = 0; i < 8; i++ )
+		buf[i] = argv[i]->i;
+
+	switch( argc ) {
+	case 8:
+		return monome_led_frame(monome, 0, buf);
+		break;
+
+	case 9:
+		return monome_led_frame(monome, argv[8]->i, buf);
+		break;
+
+	case 10:
+		/* offsets are unimplemented */
+	default:
+		break;
+	}
+
+	return -1;
+}
+
+
+static int exit_if_false(int condition) {
+	if( condition )
+		return 1;
 
 	fprintf(stderr, "aieee, could not allocate memory in "
 			"osc_register_methods(), bailing out!\n");
 	_exit(EXIT_FAILURE);
 
-	return 1;
+	return 0;
 }
 
 void osc_register_methods(sosc_state_t *state) {
@@ -78,7 +129,7 @@ void osc_register_methods(sosc_state_t *state) {
 	srv = state->server;
 
 #define METHOD(path) for( cmd_buf = NULL,\
-		 bail_if_false(asprintf(&cmd_buf, "%s/" path, prefix) > 0); \
+		 exit_if_false(asprintf(&cmd_buf, "%s/" path, prefix) > 0); \
 		 cmd_buf; free(cmd_buf), cmd_buf = NULL )
 
 #define REGISTER(typetags, cb) \
@@ -97,6 +148,21 @@ void osc_register_methods(sosc_state_t *state) {
 
 	METHOD("led")
 		REGISTER("iii", osc_led_handler);
+
+	METHOD("led_row") {
+		REGISTER("ii", osc_led_row_handler);
+		REGISTER("iii", osc_led_row_handler);
+	}
+
+	METHOD("led_col") {
+		REGISTER("ii", osc_led_col_handler);
+		REGISTER("iii", osc_led_col_handler);
+	}
+
+	METHOD("frame") {
+		REGISTER("iiiiiiii", osc_frame_handler);
+		REGISTER("iiiiiiiii", osc_frame_handler);
+	}
 
 #undef REGISTER
 #undef METHOD
