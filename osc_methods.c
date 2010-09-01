c9f975a9c8846f9ba2162f4564b203db9ec8299a
William Light <will@illest.net>
2010-09-01 02:05:17 -0700

    updating to current email address

--- osc_methods.c
+++ osc_methods.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010 William Light <will@illest.net>
+ * Copyright (c) 2010 William Light <wrl@illest.net>
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
